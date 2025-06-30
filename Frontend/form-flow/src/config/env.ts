0a0684fbd49d91fffa482452cf3d013112af6844
Evgenii <motleycrue6502@gmail.com>
2025-06-30 04:01:36 +0400

    fix

--- Frontend/form-flow/src/config/env.ts
+++ Frontend/form-flow/src/config/env.ts
@@ -1,6 +1,8 @@
 export const ENV = {
 	API_URL: process.env.REACT_APP_API_URL!,
 	SIGNALR_URL: process.env.REACT_APP_SIGNALR_URL!,
-	GOOGLE_CLIENT_ID: process.env.REACT_APP_GOOGLE_CLIENT_ID!,
-	GOOGLE_REDIRECT_URI: process.env.REACT_APP_GOOGLE_REDIRECT_URI!,
+	GOOGLE_CLIENT_ID: process.env.REACT_APP_GOOGLE_CLIENT_ID || "",
+	GOOGLE_REDIRECT_URI:
+		process.env.REACT_APP_GOOGLE_REDIRECT_URI ||
+		"http://localhost:3000/auth/google/callback",
 };
